88db284a2667436496ed5d842904bc4edefd11d1
Dennys Matthew <27568445+mclemie@users.noreply.github.com>
2022-04-22 02:22:21 +0700

    better register commands

--- scales/register.py
+++ scales/register.py
@@ -31,6 +31,7 @@ class register(Scale):
         "register", description="Register akun UCP (Max. 1 akun per discord user!)"
     )
     async def register(self, ctx):
+        connection.ping(reconnect=True)
         my_modal = Modal(
             title="Register UCP Luxinity Roleplay",
             components=[
@@ -48,27 +49,40 @@ class register(Scale):
                 ),
             ],
         )
-        await ctx.send_modal(modal=my_modal)
+        await ctx.send_modal(modal=my_modal)  # send modal to
 
-        modal_ctx: ModalContext = await self.bot.wait_for_modal(my_modal)
-        w = self.bot.get_channel(966685759832723476)
+        modal_ctx: ModalContext = await self.bot.wait_for_modal(
+            my_modal
+        )  # wait for user to enter the credentials
+
+        w = self.bot.get_channel(966685759832723476)  # get channel to send the logs
+
+        # get modal responses
         user = modal_ctx.responses["username"]
         passwd = modal_ctx.responses["password"]
+
         with connection:
             with connection.cursor() as cursor:
-                # Read a single record
                 sql = f"SELECT `discord_userid` FROM `playerucp` WHERE `discord_userid`={ctx.author.id}"
                 cursor.execute(sql)
                 result = cursor.fetchone()
                 print(result)
 
                 if result is None:
-                    hashed = bcrypt.hashpw(passwd, bcrypt.gensalt())
+                    # hash passwords
+                    hashed = bcrypt.hashpw(
+                        passwd.encode("utf8"), bcrypt.gensalt()
+                    ).decode("utf8")
 
-                    embed = dis_snek.Embed(title="", color=0x00FF00)
+                    # send embed to ucp-logs
+                    embed = dis_snek.Embed(title="New User Registered!", color=0x00FF00)
                     embed.add_field(name="Username:", value=user, inline=True)
-                    embed.add_field(name="Password:", value=f"||{passwd}||", inline=False)
-                    embed.add_field(name="Hashed Password:", value=f"||{hashed}||", inline=False)
+                    embed.add_field(
+                        name="Password:", value=f"||{passwd}||", inline=False
+                    )
+                    # embed.add_field(
+                    #    name="Hashed Password:", value=f"||{hashed}||", inline=False
+                    # )
                     embed.set_author(
                         name=f"{ctx.author.username}#{ctx.author.discriminator}",
                         url=f"https://discordapp.com/users/{ctx.author.id}",
@@ -76,20 +90,24 @@ class register(Scale):
                     )
                     embed.set_thumbnail(url=ctx.author.avatar.url)
                     embed.set_footer(
-                        text=f"{ctx.guild.name} | ID: {ctx.author.id}",
+                        text=f"{ctx.guild.name} | User ID: {ctx.author.id}",
                         icon_url=ctx.guild.icon.url,
                     )
                     embed.timestamp = datetime.datetime.utcnow()
                     await w.send(embed=embed)
+
+                    # send modal responses
                     await modal_ctx.send(
                         "Thank you for registering!",
                         ephemeral=True,
                     )
                 else:
+                    # send error message if user already registered
                     await modal_ctx.send(
                         "You're already registered! (1 UCP account per discord user)\nIf you're having trouble, contact an admin.",
                         ephemeral=True,
                     )
+
 
 def setup(bot):
     # This is called by dis-snek so it knows how to load the Scale
