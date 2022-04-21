a0599ff42c662e27b972503aadf9eac0ac6db33d
Dennys Matthew <27568445+mclemie@users.noreply.github.com>
2022-04-22 00:52:25 +0700

    genesis

---
+++ main.py
@@ -0,0 +1,107 @@
+from dis_snek import (
+    Snake,
+    Intents,
+    listen,
+    Status,
+    Scale,
+    slash_command,
+    slash_permission,
+    SlashCommandChoice,
+    Permission,
+    PermissionTypes,
+    InteractionContext,
+    slash_option,
+    OptionTypes,
+    context_menu,
+    CommandTypes,
+    Task,
+    IntervalTrigger,
+)
+from samp_client.client import SampClient
+
+import dis_snek
+import logging
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+logging.basicConfig()
+cls_log = logging.getLogger(dis_snek.const.logger_name)
+cls_log.setLevel(logging.DEBUG)
+
+
+client = Snake(
+    intents=Intents.DEFAULT,
+    sync_interactions=True,  # sync application commands with discord
+    delete_unused_application_cmds=True,  # Delete commands that arent listed here
+    asyncio_debug=True,  # Enable debug mode for asyncio
+    debug_scope=812150001089118210,  # Override the commands scope, and only create them in this guild
+)
+# during testing, we recommend setting `debug_scope`, this forces your commands to only be registered in the listed guild
+
+# load the cogs (scale)
+client.grow_scale("scales.register")
+
+
+@slash_command(
+    "regrow",
+    description="regrow a scale",
+    scopes=[812150001089118210],
+    permissions=[
+        Permission(
+            id=727798940430237757,
+            guild_id=812150001089118210,
+            type=PermissionTypes.USER,
+            permission=True,
+        ),
+        Permission(
+            id=351150966948757504,
+            guild_id=812150001089118210,
+            type=PermissionTypes.USER,
+            permission=True,
+        ),
+        Permission(
+            id=812150001089118210,
+            guild_id=812150001089118210,
+            type=PermissionTypes.ROLE,
+            permission=False,
+        ),
+    ],
+)
+@slash_option(
+    name="scale",
+    description="the scale to regrow",
+    required=True,
+    opt_type=OptionTypes.STRING,
+    choices=[
+        SlashCommandChoice(name="Register commands", value="register"),
+    ],
+)
+async def reload(ctx: InteractionContext, scale: str):
+    client.regrow_scale(f"scales.{scale}")
+    embed = dis_snek.Embed(
+        description=f"<:check:839158727512293406> **{scale}** has been successfully regrown!",
+        color=0x00FF00,
+    )
+    await ctx.send(embed=embed)
+
+
+@Task.create(IntervalTrigger(seconds=60))
+async def change_presence(self):
+    with SampClient(address=ip, port=port) as kung:
+        info = kung.get_server_info()
+    await client.change_presence(
+        status=Status.ONLINE,
+        activity=f"with {info.players}` / `{info.max_players} Players",
+    )
+
+
+@listen()  # this decorator tells snek that it needs to listen for the corresponding event, and run this coroutine
+async def on_ready():
+    # self.change_presence.start()
+    logging.info("The Bot is Ready")
+    print(f"This bot is Ready to roll and owned by {client.owner}")
+
+
+client.start(os.getenv("BOT_TOKEN"))
---
+++ scales/register.py
@@ -0,0 +1,96 @@
+from dis_snek import (
+    slash_command,
+    Modal,
+    ShortText,
+    cooldown,
+    Buckets,
+    Scale,
+)
+from dotenv import load_dotenv
+
+import os
+import bcrypt
+import pymysql.cursors
+import datetime
+import dis_snek
+
+load_dotenv()
+
+# Connect to the database
+connection = pymysql.connect(
+    host=os.getenv("DATABASE_HOST"),
+    user=os.getenv("DATABASE_USER"),
+    password=os.getenv("DATABASE_PASSWORD"),
+    database=os.getenv("DATABASE_NAME"),
+    cursorclass=pymysql.cursors.DictCursor,
+)
+
+
+class register(Scale):
+    @slash_command(
+        "register", description="Register akun UCP (Max. 1 akun per discord user!)"
+    )
+    async def register(self, ctx):
+        my_modal = Modal(
+            title="Register UCP Luxinity Roleplay",
+            components=[
+                ShortText(
+                    label="UCP Username",
+                    custom_id="username",
+                    placeholder="Masukkan username UCP kamu (contoh: Abielfrl)",
+                    required=True,
+                ),
+                ShortText(
+                    label="Password",
+                    custom_id="password",
+                    placeholder="Masukkan password UCP kamu",
+                    required=True,
+                ),
+            ],
+        )
+        await ctx.send_modal(modal=my_modal)
+
+        modal_ctx: ModalContext = await self.bot.wait_for_modal(my_modal)
+        w = self.bot.get_channel(966685759832723476)
+        user = modal_ctx.responses["username"]
+        passwd = modal_ctx.responses["password"]
+        with connection:
+            with connection.cursor() as cursor:
+                # Read a single record
+                sql = f"SELECT `discord_userid` FROM `playerucp` WHERE `discord_userid`={ctx.author.id}"
+                cursor.execute(sql)
+                result = cursor.fetchone()
+                print(result)
+
+                if result is None:
+                    hashed = bcrypt.hashpw(passwd, bcrypt.gensalt())
+
+                    embed = dis_snek.Embed(title="", color=0x00FF00)
+                    embed.add_field(name="Username:", value=user, inline=True)
+                    embed.add_field(name="Password:", value=f"||{passwd}||", inline=False)
+                    embed.add_field(name="Hashed Password:", value=f"||{hashed}||", inline=False)
+                    embed.set_author(
+                        name=f"{ctx.author.username}#{ctx.author.discriminator}",
+                        url=f"https://discordapp.com/users/{ctx.author.id}",
+                        icon_url=ctx.author.avatar.url,
+                    )
+                    embed.set_thumbnail(url=ctx.author.avatar.url)
+                    embed.set_footer(
+                        text=f"{ctx.guild.name} | ID: {ctx.author.id}",
+                        icon_url=ctx.guild.icon.url,
+                    )
+                    embed.timestamp = datetime.datetime.utcnow()
+                    await w.send(embed=embed)
+                    await modal_ctx.send(
+                        "Thank you for registering!",
+                        ephemeral=True,
+                    )
+                else:
+                    await modal_ctx.send(
+                        "You're already registered! (1 UCP account per discord user)\nIf you're having trouble, contact an admin.",
+                        ephemeral=True,
+                    )
+
+def setup(bot):
+    # This is called by dis-snek so it knows how to load the Scale
+    register(bot)
