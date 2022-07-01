c8ea67ac982e1062708f03868b650ca50eb0cb2d
Clemie McCartney <git@gnztmpz.eu.org>
2022-07-01 16:56:41 +0700

    Stats (#4)
    * add millify to deps
    
    * stats genesis
    
    * [pre-commit.ci] auto fixes from pre-commit.com hooks
    
    for more information, see https://pre-commit.ci
    
    * add character name autocomplete functions
    
    * [pre-commit.ci] auto fixes from pre-commit.com hooks
    
    for more information, see https://pre-commit.ci
    
    * add pillow to deps
    
    * use activity type for presence
    
    * add assets
    
    * [pre-commit.ci] auto fixes from pre-commit.com hooks
    
    for more information, see https://pre-commit.ci
    
    * Update blanko.png
    
    * presence import minor typofix
    
    * add current progress
    
    * remove stupid commands
    
    * [pre-commit.ci] auto fixes from pre-commit.com hooks
    
    for more information, see https://pre-commit.ci
    
    * better blanko ktp
    
    * formatted with black
    
    * finalize KTP system
    
    Co-authored-by: pre-commit-ci[bot] <66853113+pre-commit-ci[bot]@users.noreply.github.com>
    Co-authored-by: GNZTMPZ <85807297+GNZTMPZ@users.noreply.github.com>

---
+++ extensions/stats.py
@@ -0,0 +1,229 @@
+import datetime
+import os
+from io import BytesIO
+
+import aiohttp
+import naff
+import pymysql.cursors
+from dotenv import load_dotenv
+from millify import prettify
+from naff import (
+    AutocompleteContext,
+    Button,
+    ButtonStyles,
+    Embed,
+    Extension,
+    MessageTypes,
+    OptionTypes,
+    Permissions,
+    PrefixedContext,
+    check,
+    listen,
+    prefixed_command,
+    slash_command,
+    slash_option,
+)
+from PIL import Image, ImageDraw, ImageFont
+
+from utilities.checks import *
+
+load_dotenv()
+
+# Connect to the database
+connection = pymysql.connect(
+    host=os.getenv("DATABASE_HOST"),
+    user=os.getenv("DATABASE_USER"),
+    password=os.getenv("DATABASE_PASSWORD"),
+    database=os.getenv("DATABASE_NAME"),
+    cursorclass=pymysql.cursors.DictCursor,
+)
+
+
+class stats(Extension):
+    @slash_command(
+        "stats",
+        description="Show a player character stats",
+    )
+    @slash_option(
+        name="name",
+        description="The target character's name, e.g. 'Firpan_Pus'",
+        required=True,
+        opt_type=OptionTypes.STRING,
+    )
+    async def stats(self, ctx, name=str):
+        # need to be deferred, otherwise it will be failed
+        await ctx.defer()
+
+        # ping the mysql server
+        connection.ping(reconnect=True)
+
+        with connection:
+            with connection.cursor() as cursor:
+                # check if character is already registered
+                sql = f"SELECT * FROM `characters` WHERE `Name`=%s"
+                cursor.execute(sql, (name))
+                result = cursor.fetchone()
+
+                if result is None:
+                    return await ctx.send(
+                        "We can't find that character on our system, Please check for typo's or try to create a character!",
+                        ephemeral=True,
+                    )
+                else:
+                    # define things we want to use
+                    name = result["Name"]
+
+                    health = int(result["Health"])
+                    if health == 0:
+                        health = "Player is dead"
+
+                    armor = int(result["Armor"])
+                    if armor == 0:
+                        armor = "No Armor"
+                    else:
+                        armor = f"{armor}%"
+
+                    ucp = result["UCP"]
+
+                    age = result["Age"]
+
+                    origin = result["Origin"]
+
+                    gender = result["Gender"]
+                    if gender == 0:
+                        gender = "Male"
+                    else:
+                        gender = "Female"
+
+                    skin = result["Skin"]
+
+                    pocket_money = prettify(result["Money"])
+                    bank_money = prettify(result["BankMoney"])
+
+                    level = result["Level"]
+
+                    time_hour = result["Hours"]
+                    time_min = result["Minutes"]
+                    time_sec = result["Second"]
+
+                # Replace blanko.png with your background image.
+                img = Image.open("assets/blanko.png")
+                draw = ImageDraw.Draw(img)
+                # Make sure you insert a valid font from your folder.
+                values = ImageFont.truetype("assets/ARIALUNI.otf", 40)
+                date_values = ImageFont.truetype("assets/ARIALUNI.otf", 35)
+
+                #    (x,y)::↓ ↓ ↓ (text)::↓ ↓     (r,g,b)::↓ ↓ ↓
+                async with aiohttp.ClientSession() as session:
+                    async with session.get(
+                        f"https://assets.open.mp/assets//images/skins/{skin}.png"
+                    ) as response:
+                        image = await response.read()
+                avatar = (
+                    Image.open(BytesIO(image))
+                    .resize((300, 390), Image.LANCZOS)
+                    .convert("RGB")
+                )
+                img.paste(avatar, (1135, 240))
+
+                # Draw the text
+
+                # Nama
+                draw.text(
+                    (450, 255),
+                    f"{name}",
+                    (0, 0, 0),
+                    font=values,
+                )
+                # Asal/Origin
+                draw.text(
+                    (450, 305),
+                    f"{origin}",
+                    (0, 0, 0),
+                    font=values,
+                )
+                # Gender
+                draw.text(
+                    (450, 355),
+                    f"{gender}",
+                    (0, 0, 0),
+                    font=values,
+                )
+                # Pocket
+                draw.text(
+                    (450, 405),
+                    f"${pocket_money}",
+                    (0, 0, 0),
+                    font=values,
+                )
+                # Bank
+                draw.text(
+                    (450, 455),
+                    f"${bank_money}",
+                    (0, 0, 0),
+                    font=values,
+                )
+                # Ingame Time
+                draw.text(
+                    (450, 505),
+                    f"{time_hour} JAM | {time_min} MENIT | {time_hour} DETIK",
+                    (0, 0, 0),
+                    font=values,
+                )
+                # Health
+                draw.text(
+                    (450, 555),
+                    f"{health}",
+                    (0, 0, 0),
+                    font=values,
+                )
+                # Armor
+                draw.text(
+                    (450, 605),
+                    f"{armor}",
+                    (0, 0, 0),
+                    font=values,
+                )
+                # Expire Date
+                draw.text(
+                    (450, 655),
+                    f"SEUMUR HIDUP",
+                    (0, 0, 0),
+                    font=values,
+                )
+                # Date
+                draw.text(
+                    (1195, 665),
+                    f"{datetime.datetime.now().strftime('%d-%m-%Y')}",
+                    (0, 0, 0),
+                    font=date_values,
+                )
+                # Save our files.
+                img.save(f"assets/card.png")
+                ffile = naff.File(f"assets/card.png")
+                return await ctx.send(
+                    f"{ctx.author.mention}, Here's [`{name}`] character stats",
+                    file=ffile,
+                )
+
+    @stats.autocomplete("name")
+    async def stats_autocomplete(self, ctx: AutocompleteContext, name: str):
+        choices = []
+
+        # ping the mysql server
+        connection.ping(reconnect=True)
+
+        with connection:
+            with connection.cursor() as cursor:
+                # check if character is already registered
+                sql = f"SELECT `Name` FROM `characters` WHERE `Name`=%s"
+                cursor.execute(sql, (name))
+
+                for Name in cursor:
+                    choices.append({"name": f"{name}", "value": f"{name}"})
+                    await ctx.send(choices=choices)
+
+
+def setup(bot):
+    # This is called by dis-snek so it knows how to load the Extension
+    stats(bot)
--- main.py
+++ main.py
@@ -26,62 +26,6 @@ if __name__ == "__main__":
         debug_scope=812150001089118210,  # Override the commands scope, and only create them in this guild
     )
 
-    # bot owner only commands
-
-    @prefixed_command(name="load")
-    @check(is_owner())
-    async def load(ctx: PrefixedContext, scale: str):
-        bot.load_extension(f"scales.{scale}")
-        embed = Embed(
-            description=f"<:check:839158727512293406> **{scale}** has been successfully growed!",
-            color=0x00FF00,
-        )
-        await ctx.send(embed=embed)
-
-    @prefixed_command(name="unload")
-    @check(is_owner())
-    async def unload(ctx: PrefixedContext, scale: str):
-        bot.unload_extension(f"scales.{scale}")
-        embed = Embed(
-            description=f"<:check:839158727512293406> **{scale}** has been successfully ungrowed!",
-            color=0x00FF00,
-        )
-        await ctx.send(embed=embed)
-
-    @prefixed_command(name="reload")
-    @check(is_owner())
-    async def reload(ctx: PrefixedContext, scale: str):
-        bot.reload_extension(f"scales.{scale}")
-        embed = Embed(
-            description=f"<:check:839158727512293406> **{scale}** has been successfully regrowed!",
-            color=0x00FF00,
-        )
-        await ctx.send(embed=embed)
-
-    def restart_program():
-        python = sys.executable
-        os.execl(python, python, *sys.argv)
-
-    @prefixed_command(name="reboot")
-    @check(is_owner())
-    async def reboot(ctx: PrefixedContext):
-        embed = Embed(
-            description=f":wave: The bot has been Rebooting, Please Wait..",
-            color=0xE74C3C,
-        )
-        await ctx.send(embed=embed)
-        restart_program()
-
-    @prefixed_command(name="shutdown")
-    @check(is_owner())
-    async def shutdown(ctx: PrefixedContext):
-        embed = Embed(
-            description=f":wave: The bot has been Shutdowned, Goodbye World.",
-            color=0xE74C3C,
-        )
-        await ctx.send(embed=embed)
-        await bot.stop()
-
     # load the debug extension if that is wanted
     if os.getenv("LOAD_DEBUG_COMMANDS") == "true":
         DebugExtension(bot=bot)
